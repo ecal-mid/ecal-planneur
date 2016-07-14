17557637e5d1278432dcefe75375dc9e2ed115f8
Cyril Diagne <cyril.diagne@ecal.ch>
2016-07-14 22:47:32 +0200

    add report token

--- src/index.py
+++ src/index.py
@@ -1,6 +1,7 @@
 """ Index """
 
 from flask import Blueprint, render_template, jsonify
+from itsdangerous import URLSafeSerializer
 
 from .models import planning
 
@@ -15,10 +16,9 @@ def index():
     return render_template('index.html', planning=planning)
 
 @bp.route('/detail/<string:staff>')
-def index_staff(staff):
+def report_staff(staff):
     """Return the homepage with access to a staff's details."""
     staff = planning.get_staff_by_name(staff)
-    print staff
     return render_template('index.html', planning=planning, detail=staff, detail_json=staff)
 
 @bp.route('/admin')
@@ -30,3 +30,17 @@ def admin():
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
+
+@bp.route('/report/<string:token>')
+def token_access(token):
+    secret = open('config/'+planning.config.name+'/secret.yaml')
+    s = URLSafeSerializer(secret.read())
+    try:
+        name = s.loads(token)
+        if name in [x.name for x in planning.staffs]:
+            return report_staff(name)
+    except:
+        print 'token error'
+    for name in [x.name for x in planning.staffs]:
+        print name, s.dumps(name)
+    return 'token not recognized or expired'
