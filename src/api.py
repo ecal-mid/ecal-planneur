d730a95d22ad381c3f8782058bcb3c03cf916cd5
Cyril Diagne <cyril.diagne@ecal.ch>
2016-07-15 12:15:39 +0200

    manually cache activities to reduce DataStore quota usage and remain in the 0.05M reads free tier

--- src/api.py
+++ src/api.py
@@ -13,6 +13,17 @@ from .models import Planning, planning, Activity
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
+activities = {}
+activities_cache_need_update = True
+
+def update_activity_cache():
+    global activities
+    activities = {}
+    ancestor_key = ndb.Key("Planning", Planning.config.name)
+    act = Activity.query(ancestor=ancestor_key).fetch()
+    for a in act:
+        activities[a.key.id()] = a
+
 @bp.route('/')
 def hello():
     return 'API v1'
@@ -23,10 +34,14 @@ def get_staff(name):
     return jsonify(staff)
 
 @bp.route('/activity')
-def get_activity():
-    ancestor_key = ndb.Key("Planning", Planning.config.name)
-    activities = Activity.query(ancestor=ancestor_key).fetch()
-    result = [a.get_dict() for a in activities]
+def get_activities():
+    # update cache if necessary
+    global activities_cache_need_update
+    if activities_cache_need_update is True:
+        update_activity_cache()
+        activities_cache_need_update = False
+    # return results from cache
+    result = [act.get_dict() for key, act in activities.iteritems()]
     return jsonify(result)
 
 @bp.route('/activity', methods=['POST'])
@@ -40,6 +55,9 @@ def add_activity():
     activity.date = datetime.strptime(request.form['date'], date_format)
     activity.is_pm = (int(request.form['is_pm']) == 1)
     activity.put()
+    # update cache
+    aid = activity.key.id()
+    activities[aid] = activity
     # get updated staff
     staff = planning.get_staff_by_name(activity.staff)
     return jsonify({
@@ -53,6 +71,8 @@ def delete_activity(id):
     activity = Activity.get_by_id(id, parent=ancestor_key)
     staff_name = activity.staff
     activity.key.delete()
+    # update cache
+    del activities[id]
     # get updated staff
     staff = planning.get_staff_by_name(staff_name)
     return jsonify(staff)
