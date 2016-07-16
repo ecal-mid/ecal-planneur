73a84194a9834c49c39dffbfb28657be0a39c844
Cyril Diagne <cyril.diagne@ecal.ch>
2016-07-16 12:39:03 +0200

    fix activity initials building function

--- static/js/activity.js
+++ static/js/activity.js
@@ -55,7 +55,7 @@ class Activity {
   addView() {
     var query = 'td[data-date="'+ this.getDateLabel()+'"].' + (this.isPm?'pm':'am');
     var td = document.querySelector(query);
-    var initials = this.staff.split(' ').map( function(x) { x[0] } ).join('');
+    var initials = this.staff.split(' ').map(function(x) { return x[0]; }).join('');
     var color = 'color-' + this.task.substr(0,3);
 
     var el = document.createElement('span');
