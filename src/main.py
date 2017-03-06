b39c3ea65af43eeef225f6aff49d0a4279b8d985
Cyril Diagne <cyril.diagne@ecal.ch>
2017-03-06 10:59:15 +0100

    begin caldav sync

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 from .index import bp as index
 from .api import bp as api
-from .caldav import bp as caldav
+from .calsync import bp as calsync
 from .changelist import bp as changelist
 
 app = Flask(__name__)
@@ -12,5 +12,5 @@ app = Flask(__name__)
 
 app.register_blueprint(index)
 app.register_blueprint(api)
-app.register_blueprint(caldav)
+app.register_blueprint(calsync)
 app.register_blueprint(changelist)
